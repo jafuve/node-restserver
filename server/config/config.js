5283ab17bc34f65199a419e3d77e19adcc36fa0c
Jairo Fuentes <jairo_fuentesv@hotmail.com>
2019-12-01 11:10:22 -0600

    Otro cambio

--- server/config/config.js
+++ server/config/config.js
@@ -13,13 +13,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ==========================
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = 'mongodb+srv://jafuve:jafuve@clusterjafuve-z70eu.mongodb.net/test?retryWrites=true&w=majority';
-}
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+//     urlDB = 'mongodb+srv://jafuve:jafuve@clusterjafuve-z70eu.mongodb.net/test?retryWrites=true&w=majority';
+// }
 
-// urlDB = 'mongodb+srv://jafuve:jafuve@clusterjafuve-z70eu.mongodb.net/test?retryWrites=true&w=majority';
+urlDB = 'mongodb+srv://jafuve:jafuve@clusterjafuve-z70eu.mongodb.net/test?retryWrites=true&w=majority';
 
 //este env esinventado
 process.env.URLDB = urlDB;
