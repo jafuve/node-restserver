8a01f30dfec1b0bed9f2fb7dcc2e9029c20073e4
Jairo Fuentes <jairo_fuentesv@hotmail.com>
2019-12-01 10:52:14 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // Puerto
 // ==========================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==========================
+// Entorno
+// ==========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==========================
+// Entorno
+// ==========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jafuve:jafuve@clusterjafuve-z70eu.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+// urlDB = 'mongodb+srv://jafuve:jafuve@clusterjafuve-z70eu.mongodb.net/test?retryWrites=true&w=majority';
+
+//este env esinventado
+process.env.URLDB = urlDB;
