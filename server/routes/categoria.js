9dd82272ffb7cd6d1a4d97c80d5699aae0b51d08
Jairo Fuentes <jairo_fuentesv@hotmail.com>
2019-12-04 21:34:07 -0600

    Productos y categorias

---
+++ server/routes/categoria.js
@@ -0,0 +1,157 @@
+const express = require('express');
+
+let { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion');
+
+let app = express();
+
+let Categoria = require('../models/categoria');
+
+// =============
+//Mostrar todas las categorias
+// =============
+app.get('/categoria', verificaToken, (req, res) => {
+    Categoria.find({})
+        .sort('descripcion')
+        .populate('usuario', 'nombre email')
+        .exec((err, categorias) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                categorias
+            });
+        });
+});
+
+// =============
+//Mostrar todas las categorias
+// =============
+app.get('/categoria/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El id no es correcto'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// =============
+// Crear nueva categoria
+// =============
+app.post('/categoria', verificaToken, (req, res) => {
+    let body = req.body;
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    });
+
+    categoria.save((err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// =============
+// Crear nueva categoria
+// =============
+app.put('/categoria/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+
+    let descCategoria = {
+        descripcion: body.descripcion
+    };
+
+    Categoria.findByIdAndUpdate(id, descCategoria, { new: true, runValidators: true }, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// =============
+// Delete categoria
+// =============
+app.delete('/categoria/:id', [verificaToken, verificaAdminRole], (req, res) => {
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'el id no existe'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            message: 'Categoria borrada'
+        });
+    });
+});
+
+module.exports = app;
